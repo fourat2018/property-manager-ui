6b066f6edd32a781a97958455730d75c6bc05b36
fourat1991 <euphrate1991@gmail.com>
2018-05-08 09:22:50 +0200

    Basic UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PropertyComponent } from './property/property.component';
+
+ import { HttpModule } from '@angular/http';
+ import { HttpClientModule, HttpClient } from '@angular/common/http';
+
+import { AppRoutingModule }     from './app-routing.module';
+
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
